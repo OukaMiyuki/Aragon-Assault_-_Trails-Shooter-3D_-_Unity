9f730c1f266964630c25f3863f8991108f805ebf
OukaMiyuki <asukaloid.blog@gmail.com>
2020-08-31 21:03:25 +0700

    Second Commit : Add Player ship, Obstacles, Landing Pad, Gameplay Music, Control Script & Scene Loader

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerControl : MonoBehaviour {
+
+    //[SerializeField] GameObject[] guns;
+
+    [Tooltip("In ms")] [SerializeField] float Speed = 20f;
+    [Tooltip("In meter")] [SerializeField] float xRange = 5.5f;
+    [Tooltip("In meter")] [SerializeField] float yRange = 3.5f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+
+    [SerializeField] float positionYawFactor = 5f;
+
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow;
+    float yThrow;
+
+    bool isControlEnabled = true; //control enable = player alive, control disabled = player dead or colliding
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled) {
+            ProcessMovement();
+            ProcessRotation();
+            ProcessFiringBullets();
+        }
+    }
+
+    private void StopMovementOnDeath() {
+        isControlEnabled = false;
+    }
+
+    private void ProcessMovement() {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float yOffset = yThrow * Speed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);//move the ship
+    }
+
+    private void ProcessRotation() {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        //float yaw = (transform.localPosition.x * Time.deltaTime); //y axes (left right)
+        float roll = xThrow * controlRollFactor; //z axes (wing left and right)
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessFiringBullets() {
+        //if (CrossPlatformInputManager.GetButton("Fire")) {
+         //   fireAttack();
+        //} else {
+         //   disableFire();
+        //}
+    }
+
+    private void fireAttack() {
+        //foreach (GameObject gun in guns) {
+         //   gun.SetActive(true);
+        //}
+    }
+
+    private void disableFire() {
+        //foreach (GameObject gun in guns) {
+         //   gun.SetActive(false);
+        //}
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Feel free to donwload the project and use whatever you want to use.
 
 ## Project Timeline
 
-1. Sunday, August 30, 2020 : 23:56 => Import some textures and Skybox from unity assets store [texture link](https://assetstore.unity.com/packages/2d/textures-materials/24-pbr-materials-for-unity-5-51991) | [Skybox link](https://assetstore.unity.com/packages/2d/textures-materials/sky/skybox-volume-2-nebula-3392)
+1. Sunday, August 30, 2020 - 23:56 => Import some textures and Skybox from unity assets store [texture link](https://assetstore.unity.com/packages/2d/textures-materials/24-pbr-materials-for-unity-5-51991) | [Skybox link](https://assetstore.unity.com/packages/2d/textures-materials/sky/skybox-volume-2-nebula-3392)
+
+2. Monday, August 31, 2020 - 20:42 => Added some obstacles, player ship, landing pad, splash scene, gameplay music, scene loader scripts & player control movement (also use cross-platform input from unity standart assets) | [Unity standart assets](https://assetstore.unity.com/packages/essentials/asset-packs/standard-assets-for-unity-2018-4-32351) |
+[Rock assets link1](https://assetstore.unity.com/packages/3d/props/exterior/rock-package-118182) | [Player Ship](https://assetstore.unity.com/packages/3d/vehicles/space/star-sparrow-modular-spaceship-73167) | [Rock assets link 2](https://assetstore.unity.com/packages/3d/props/exterior/hq-rock-pack-free-83388) | [Landing Pad](https://assetstore.unity.com/packages/3d/environments/urban/low-poly-street-pack-67475) (not actually a landing pad assets but hey, pretty much close right? I'm not an artist btw so yeah...)
